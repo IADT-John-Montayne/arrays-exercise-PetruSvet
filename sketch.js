04c689f406a7b483c2d48fc994ee60dcef0019d8
PetruSvet <n00243240@iadt.ie>
2026-01-20 12:39:19 +0000

    20/01/2026

--- sketch.js
+++ sketch.js
@@ -7,3 +7,45 @@ function setup() {
 function draw() {
 	rect(250, 250, 100, 100);
 }
+
+
+// Loop an array
+let fruits = ["apple", "banana", "orange"]; 
+
+for (let i = 0; i < fruits.length; i++) {
+	console.log(fruits[i]);
+}
+// apple,banana,orange
+
+
+//Looping with for..of
+let fruits2 = ["apple", "banana", "orange"]; 
+
+for (let fruit of fruits2) {
+	console.log(fruit);
+}
+// apple,banana,orange
+
+
+// Slicing an array
+let names = ["John", "Michael", "Andrew", "Billy"]
+ 
+let group = names.slice(1,3);
+console.log(group);
+// michael,andrew
+
+
+// Removing/Replacing - splice()
+let cars = ["Honda", "Toyota", "Nissan"]
+cars.splice(1,1);
+console.log(cars);
+// ["Honda", "Toyota"]
+
+//Replacing with splice()
+let vegetables = ["Tomato", "Cucumber", "Bell Pepper"]
+fruits.splice(1,1, "cabbage");
+console.log(vegetables);
+// ["Tomato, "cabbage", "Bell Pepper"]
+
+
+
